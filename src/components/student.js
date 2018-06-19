ae1ba918154e9fdeafbf9735b8944fd707f5b0b1
Bhakti Wakchaure <bhakti.wakchaure@gmail.com>
2018-06-19 10:28:31 -0700

    added students

--- src/components/student.js
+++ src/components/student.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const student = () =>{
 
-return <h3>Student list</h3>
+return <div>
+<h3>Student list</h3>
+<div>1</div>
+<div>2</div>
+<div>3</div>
+<div>4</div>
+</div>
 
 }
 
